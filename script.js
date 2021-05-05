5143a206e806c5cac07f19d0ec2cacf347703d14
BlueKetchup07 <66294851+BlueKetchup07@users.noreply.github.com>
2021-05-05 10:06:46 +0530

    THE CATS NOW ANIMATED

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var contact = document.getElementsByClassName("working-button-contact")[0];
 
 contact.addEventListener("click",function(){
-    location.href = "https://us1.list-manage.com/contact-form?u=b3ef7a893b580ae2150ab4827&form_id=e2f48a5a7ab3b266135899b3e9dea0b8"
+    location.href = "https://us1.list-manage.com/contact-form?u=b3ef7a893b580ae2150ab4827&form_id=e2f48a5a7ab3b266135899b3e9dea0b8";
 })
