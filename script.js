6d59f42651e270b96261a154b91995771a2d697b
BlueKetchup07 <66294851+BlueKetchup07@users.noreply.github.com>
2021-05-06 09:42:29 +0530

    nothing

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var contact = document.getElementsByClassName("working-button-contact")[0];
+var redirecttomovie = document.getElementById("movie");
 
 contact.addEventListener("click",function(){
     location.href = "https://us1.list-manage.com/contact-form?u=b3ef7a893b580ae2150ab4827&form_id=e2f48a5a7ab3b266135899b3e9dea0b8";
+})
+
+redirecttomovie.addEventListener("click",function(){
+    location.href = "https://en.wikipedia.org/wiki/Star_Wars:_Episode_III_%E2%80%93_Revenge_of_the_Sith";
 })
